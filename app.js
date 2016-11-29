83b653dc4dda71e1af64c059f323179ea9b13508
Sravan921 <sukhavasi92@gmail.com>
2016-11-28 22:57:52 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// We are creating a nested object here.
+
+ var person = {
+    name:"Sravan Kumar",
+	age:25,
+	salary:50000,
+   address: {
+    city:"Omaha",
+	state:"Nebraska",
+	zipcode:"68135"
+	}
+}; 
